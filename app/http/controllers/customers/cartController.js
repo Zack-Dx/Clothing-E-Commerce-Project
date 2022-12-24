b1b4041a7b934e0ceb85ad4dccb0612a998848d7
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-12-24 15:11:32 +0530

    Cart Page Section updated.

--- app/http/controllers/customers/cartController.js
+++ app/http/controllers/customers/cartController.js
@@ -1,39 +1,39 @@
 function cartController() {
-  return {
-    //Get
-    cart(req, res) {
-      res.render("purchase/cart");
-    },
+    return {
+        //Get
+        cart(req, res) {
+            res.render('purchase/cart', { name: 'Animesh' });
+        },
 
-    //POST
-    update(req, res) {
-      // For the first time
-      if (!req.session.cart) {
-        req.session.cart = {
-          items: {},
-          totalQty: 0,
-          totalPrice: 0,
-        };
-      }
+        //POST
+        update(req, res) {
+            // For the first time
+            if (!req.session.cart) {
+                req.session.cart = {
+                    items: {},
+                    totalQty: 0,
+                    totalPrice: 0,
+                };
+            }
 
-      // If item doen't exist in cart
-      let cart = req.session.cart;
-      if (!cart.items[req.body._id]) {
-        cart.items[req.body._id] = {
-          item: req.body,
-          qty: 1,
-        };
-        (cart.totalQty = cart.totalQty + 1),
-          (cart.totalPrice = cart.totalPrice + req.body.price);
-      } else {
-        cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1;
-        (cart.totalQty = cart.totalQty + 1),
-          (cart.totalPrice = cart.totalPrice + req.body.price);
-      }
+            // If item doen't exist in cart
+            let cart = req.session.cart;
+            if (!cart.items[req.body._id]) {
+                cart.items[req.body._id] = {
+                    item: req.body,
+                    qty: 1,
+                };
+                (cart.totalQty = cart.totalQty + 1),
+                    (cart.totalPrice = cart.totalPrice + req.body.price);
+            } else {
+                cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1;
+                (cart.totalQty = cart.totalQty + 1),
+                    (cart.totalPrice = cart.totalPrice + req.body.price);
+            }
 
-      return res.json({ totalQty: req.session.cart.totalQty });
-    },
-  };
+            return res.json({ totalQty: req.session.cart.totalQty });
+        },
+    };
 }
 
 export { cartController };
