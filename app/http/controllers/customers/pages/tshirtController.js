0228d50355b1e1e1eaef20240f7da1dc9abfe4f9
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-01-07 20:04:40 +0530

    Fixes.

--- app/http/controllers/customers/pages/tshirtController.js
+++ app/http/controllers/customers/pages/tshirtController.js
@@ -4,7 +4,7 @@ function tshirtController() {
     async index(req, res) {
       try {
         const products = await Tees.find();
-        res.render("customers/tshirt", { products});
+        res.render("customers/tshirt", { products });
       } catch (error) {
         console.log(error);
       }
@@ -12,4 +12,4 @@ function tshirtController() {
   };
 }
 
-export { tshirtController};
+export { tshirtController };
